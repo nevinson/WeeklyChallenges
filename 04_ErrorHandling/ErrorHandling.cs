848cfb25fb7e0af9cfc2045ac1dcc2f974c2dda7
Nevinson <nevinson23@gmail.com>
2014-09-04 23:49:14 +0200

    04_Error Handling
    Add logic on exception handling.

---
+++ 04_ErrorHandling/ErrorHandling.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_ErrorHandling
+{
+    public class ErrorHandling
+    {
+        public int GrandparentMethod(int position)
+        {
+            int output = 0;
+
+            Console.WriteLine("Open Database Connection");
+
+            try
+            {
+                output = ParentMethod(position);
+
+                //if (position < 0)
+                //{
+                //    throw new IndexOutOfRangeException("The value must be >= 0");
+                //}
+            }
+            catch (Exception ex)
+            {
+                // Do some logging
+                throw new ArgumentException("You passed in bad data", ex);
+            }
+            finally
+            {
+                Console.WriteLine("Close Database Connection");
+            }
+
+            return output;
+        }
+
+        public int ParentMethod(int position)
+        {
+            return GetNumber(position);
+        }
+
+        public int GetNumber(int position)
+        {
+            int output = 0;
+
+            //try
+            //{
+            int[] numbers = new int[] { 1, 4, 7, 2 };
+            output = numbers[position];
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //    Console.WriteLine(ex.StackTrace);
+            //}
+
+            return output;
+        }
+    }
+}
---
+++ 04_ErrorHandling/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_ErrorHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ErrorHandling _eerorHandler = new ErrorHandling();
+
+            try
+            {
+                int result = _eerorHandler.GrandparentMethod(4);
+                Console.WriteLine($"The value at the given position is { result }");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("You gave us bad information. Bad user!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+                var inner = ex.InnerException;
+
+                while (inner != null)
+                {
+                    Console.WriteLine(inner.StackTrace);
+                    inner = inner.InnerException;
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
