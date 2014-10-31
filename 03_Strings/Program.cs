b82d4a62cd2eb910d5254f469a84279e68c2d6d0
Nevinson <nevinson23@gmail.com>
2014-10-31 21:08:59 +0200

    02_Strings
    Read text input, remove vowels and set the first letter to uppercase.

---
+++ 03_Strings/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read text input, remove vowels and set the first letter to uppercase
+            var input = Console.ReadLine();
+            var vowels = "AaEeIiOoUu";
+            if (string.IsNullOrEmpty(input))
+            {
+                Console.WriteLine("Please enter a sentence to continue\n");
+            }
+            else
+            {
+                input = new string(input.Where(m => !vowels.Contains(m)).ToArray());
+                input = input.First().ToString().ToUpper() + input.Substring(1);
+
+                Console.WriteLine($"Result after vowels removed: {input}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
