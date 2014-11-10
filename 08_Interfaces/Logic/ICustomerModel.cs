4b0fe629afdba28c404ef52478fa252a98ed032c
Nevinson <nevinson23@gmail.com>
2014-11-10 02:03:11 +0200

    08_Interfaces

---
+++ 08_Interfaces/Logic/ICustomerModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Interfaces.Logic
+{
+    public interface ICustomerModel
+    {
+        string City { get; set; }
+        string EmailAddress { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string PhoneNumber { get; set; }
+        string StreetAddress { get; set; }
+    }
+}
---
+++ 08_Interfaces/Program.cs
@@ -0,0 +1,55 @@
+﻿using _08_Interfaces.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IProductModel> cart = AddSampleData();
+            CustomerModel customer = GetCustomer();
+
+            foreach (IProductModel prod in cart)
+            {
+                prod.ShipItem(customer);
+
+                if (prod is IDigitalProductModel digital)
+                {
+                    Console.WriteLine($"For the { digital.Title } you have { digital.TotalDownloadsLeft } downloads left.");
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+        private static CustomerModel GetCustomer()
+        {
+            return new CustomerModel
+            {
+                FirstName = "Nevinson",
+                LastName = "Moyo",
+                City = "Johannesburg",
+                EmailAddress = "me@nevinson.com",
+                PhoneNumber = "073-602-2982"
+            };
+        }
+
+        private static List<IProductModel> AddSampleData()
+        {
+            List<IProductModel> output = new List<IProductModel>();
+
+            output.Add(new PhysicalProductModel { Title = "Nerf Football" });
+            output.Add(new PhysicalProductModel { Title = "Nevinson T-Shirt" });
+            output.Add(new PhysicalProductModel { Title = "Hard Drive" });
+            output.Add(new DigitalProductModel { Title = "Lesson Source Code" });
+            output.Add(new CourseProductModel { Title = ".NET Core Start to Finish" });
+
+            return output;
+        }
+    }
+}
