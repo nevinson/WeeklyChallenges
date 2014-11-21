1dffb0b4a86732962291a902080da6818fadad21
Nevinson <nevinson23@gmail.com>
2014-11-21 16:41:21 +0200

    09_AbstractClass

---
+++ 09_AbstractClass/Program.cs
@@ -0,0 +1,32 @@
+﻿using _09_AbstractClass.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<DataAccess> databases = new List<DataAccess>()
+            {
+                new SqlDataAccess(),
+                new SqliteDataAccess()
+            };
+
+            foreach (var db in databases)
+            {
+                Console.WriteLine(db.LoadConnectionString("demo"));
+                //db.LoadConnectionString("demo");
+                //db.LoadData("select * from table");
+                //db.SaveData("insert into table");
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
